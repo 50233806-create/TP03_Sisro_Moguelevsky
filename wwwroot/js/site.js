02a2403abd4da3c302e741a8cebee6a8ebdad713
50233806-create <50233806@est.ort.edu.ar>
2026-06-03 08:36:19 -0300

    Refactor Ahorcado function and remove ArriesgarLetra

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿function ArriesgarLetra()
- {
-   let letra = document.getElementByID("Letra");
- }
+let intentos = 10;
+function Ahorcado()
+{
+    let input = document.getElementById("letra");
+    let letra = input.value.toUpperCase();
+    let palabra = document.getElementById("palabraReal").value;
+    let palabraOculta =document.getElementById("palabraOculta").innerHTML;
+    let nuevaPalabra = "";
+    let acerto = false;
+    if(letra == "" || letra.length>1)
+    {
+    document.getElementById("msj").innerHTML="Ingrese una sola letra"
+    return;
+    }
+    for(let i = 0; i < palabra.length; i++)
+    {
+        if(palabra [i] == letra)
+        {
+            nuevaPalabra += letra + " ";
+            acerto = true;
+            i++;
+        }
+        else(!acerto)
+        {
+        intentos--;
+        document.getElementById("intentos").innerHTML=intentos;
+        }
+    }   
+    if(acerto==palabra.length)
+    {
+    document.getElementById("mensaje").innerHTML ="GANASTE";
+    }
+    if(intentos== 0)
+    {
+    document.getElementById("mensaje").innerHTML ="PERDISTE";
+    document.getElementById("palabraOculta").innerHTML = palabra;
+    }
+    input.value = "";
+}
