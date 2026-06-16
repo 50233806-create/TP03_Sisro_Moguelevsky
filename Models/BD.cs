a7634d2efdcba348f8a7e3de14d333a02a6ca66e
AlejandroSisro <50434059@est.ort.edu.ar>
2026-06-16 08:38:56 -0300

    Refactor BD class and add RecibirPalabras method

--- Models/BD.cs
+++ Models/BD.cs
@@ -4,5 +4,14 @@ using Dapper;
 namespace TP03_Moguelevsky_Sisro.Models;
 public class BD
 {
+    private string connectionString = @"Server=localhost; DataBase AhorcadoCinematico; Integrated Security=True; Trust Server Certificate=True;";
 
+    public List<string> RecibirPalabras()
+    {
+        List<string> palabras = new List<string>();
+        using(SqlConnection connection = new SqlConnection(_connectionString))
+        string query = "SELECT palabra FROM Palabras";
+        palabras = connection.Query<Patente>(query).ToList();
+        return palabras;
+    }
 }
