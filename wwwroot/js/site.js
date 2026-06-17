22302cfbe1d0b31ed8851cbc652c997c478f402a
50233806-create <50233806@est.ort.edu.ar>
2026-06-17 08:32:15 -0300

    Refactor ArriesgarLetra function and intentos logic

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,37 +1,45 @@
-    let intentos = 10;
-function ArriesgarLetra()
-{
+let intentos = 10;
+
+function ArriesgarLetra() {
 
     let input = document.getElementById("letra");
     let letra = input.value.toUpperCase();
-    let palabra = document.getElementById("palabraReal").innerHTML;
-    let palabraOculta = document.getElementById("palabraOculta").innerHTML;
+
+    if (letra === "" || letra.length > 1) {
+        document.getElementById("msj").innerHTML =
+            "Ingrese una sola letra";
+        return;
+    }
+
+    let palabra =
+        document.getElementById("palabraReal").innerHTML.toUpperCase();
+
     let nuevaPalabra = "";
     let acerto = false;
-    if(letra == "" || letra.length>1)
-    {
-    document.getElementById("msj").innerHTML="Ingrese una sola letra"
-    return;
-    }
-    for(let i = 0; i < palabra.length; i++)
-    {
-        if(palabra [i] == letra)
-        {
+
+    for (let i = 0; i < palabra.length; i++) {
+
+        if (palabra[i] === letra) {
             nuevaPalabra += letra + " ";
-            document.getElementById("M").innerHTML ="Acertaste ";
-            document.getElementById("PalabraConLetraAdivinada").innerHTML= nuevaPalabra;       
-            i++;
+            acerto = true;
+        } else {
+            nuevaPalabra += "_ ";
         }
-        else
-        {
-        intentos=inentos-1;
-        document.getElementById("intentos").innerHTML=intentos;
-        }
-    }   
-    if(intentos==0)
-    {
-    document.getElementById("mensaje").innerHTML ="PERDISTE";
-    document.getElementById("palabraOculta").innerHTML = palabra;
     }
+
+    if (acerto) {
+        document.getElementById("M").innerHTML = "Acertaste";
+        document.getElementById("PalabraConLetraAdivinada").innerHTML =
+            nuevaPalabra;
+    } else {
+        intentos--;
+        document.getElementById("intentos").innerHTML = intentos;
+    }
+
+    if (intentos === 0) {
+        document.getElementById("mensaje").innerHTML = "PERDISTE";
+        document.getElementById("palabraOculta").innerHTML = palabra;
+    }
+
     input.value = "";
 }
